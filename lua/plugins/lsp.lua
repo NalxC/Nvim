1ad7a5328211bb9dde2def7a7a161cc7e80fc3c4
NalxC <you@example.com>
2023-08-13 18:06:54 +0800

    add lsp key-mapping

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -38,11 +38,46 @@ return {
                   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
                 end
             end
-            -- sign-icon
 
+            -- on_attach
+            local on_attach = function (_, bufnr)
+               local key_map = {
+                   ["n"] = {
+                      ["K"] = { "<cmd>lua vim.lsp.buf.hover()<cr>", "Show hover" },
+                      ["gd"] = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Goto definition" },
+                      ["gD"] = { "<cmd>lua vim.lsp.buf.declaration()<cr>", "Goto Declaration" },
+                      ["gr"] = { "<cmd>lua vim.lsp.buf.references()<cr>", "Goto references" },
+                      ["gI"] = { "<cmd>lua vim.lsp.buf.implementation()<cr>", "Goto Implementation" },
+                      ["gs"] = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "show signature help" },
+                      ["gl"] = {
+                        function()
+                          local float = vim.diagnostic.config().float
+
+                          if float then
+                            local config = type(float) == "table" and float or {}
+                            config.scope = "line"
+
+                            vim.diagnostic.open_float(config)
+                          end
+                        end,
+                        "Show line diagnostics",
+                      },
+                   },
+               }
+
+               for mode, mapping in pairs(key_map) do
+                   for key, remap in pairs(mapping) do
+                       local opts = { buffer = bufnr, desc = remap[2], noremap = true, silent = true }
+                       vim.keymap.set(mode, key, remap[1], opts)
+                   end
+               end
+            end
+
+            -- setup
             for _, server in pairs(servers) do
                 lspconfig[server].setup {
                     capabilities = capabilities,
+                    on_attach = on_attach,
                 }
             end
         end
