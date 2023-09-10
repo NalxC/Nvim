4fe08f1d8ff5f9d92c0ce5068448c13b9e1df095
NalxC <18287509+NalxC@users.noreply.github.com>
2023-09-11 03:15:33 +0800

    config update.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,12 +1,11 @@
-local opts = { noremap = true, silent = true }
-
-local term_opts = { silent = true }
-
 -- Shorten function name
 local keymap = vim.keymap.set
 
+local opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
+
+
 --Remap space as leader key
--- keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,55 +1,42 @@
-local options = {
-  backup = false,                          -- creates a backup file
-  -- swapfile = false,                        -- creates a swapfile
-  -- updatetime = 300,                        -- faster completion (4000ms default)
-  cmdheight = 1,                           -- more space in the neovim command line for displaying messages
-  --completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-  --conceallevel = 0,                        -- so that `` is visible in markdown files
-  fileencoding = "utf-8",                  -- the encoding written to a file
-  hlsearch = true,                         -- highlight all matches on previous search pattern
-  ignorecase = true,                       -- ignore case in search patterns
-  smartcase = true,                        -- smart case
-  mouse = "nv",                             -- allow the mouse to be used in neovim
-  pumheight = 10,                          -- pop up menu height
-  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
-  -- showtabline = 2,                         -- always show tabline
-  smartindent = true,                      -- make indenting smarter again 自动缩进
-  splitbelow = true,                       -- force all horizontal splits to go below current window
-  splitright = true,                       -- force all vertical splits to go to the right of current window
-  -- termguicolors = true,                    -- set term gui colors (most terminals support this)
-  timeoutlen = 1000,                        -- time to wait for a mapped sequence to complete (in milliseconds)
-  undofile = true,                         -- enable persistent undo
-  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  expandtab = true,                        -- convert tabs to spaces
-  shiftwidth = 4,                          -- the number of spaces inserted for each indentation
-  tabstop = 4,                             -- insert 2 spaces for a tab
-  cursorline = true,                       -- highlight the current line
-  number = true,                           -- set numbered lines
-  relativenumber = true,                  -- set relative numbered lines
-  numberwidth = 4,                         -- set number column width to 2 {default 4}
+local opt = vim.opt
 
-  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
-  wrap = true,                             -- display lines as one long line
-  linebreak = true,                        -- companion to wrap, don't split words
-  scrolloff = 8,                           -- minimal number of screen lines to keep above and below the cursor
-  sidescrolloff = 8,                       -- minimal number of screen columns either side of cursor if wrap is `false`
-  --guifont = "monospace:h17",               -- the font used in graphical neovim applications
-  whichwrap = "bs<>[]hl",                  -- which "horizontal" keys are allowed to travel to prev/next line
-  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
-}
+opt.backup = false                          -- creates a backup file
+opt.updatetime = 300                        -- faster completion (4000ms default)
+opt.cmdheight = 1                           -- more space in the neovim command line for displaying messages
+opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
+-- opt.conceallevel = 0                        -- so that `` is visible in markdown files
+opt.fileencoding = "utf-8"                  -- the encoding written to a file
+opt.hlsearch = true                         -- highlight all matches on previous search pattern
+opt.ignorecase = true                       -- ignore case in search patterns
+opt.smartcase = true                        -- smart case
+opt.mouse = "nv"                             -- allow the mouse to be used in neovim
+opt.pumheight = 10                          -- pop up menu height
+opt.showmode = false                        -- we don't need to see things like -- INSERT -- anymore
+-- opt.showtabline = 2                         -- always show tabline
+opt.smartindent = true                      -- make indenting smarter again 自动缩进
+opt.splitbelow = true                       -- force all horizontal splits to go below current window
+opt.splitright = true                       -- force all vertical splits to go to the right of current window
+opt.termguicolors = true                    -- set term gui colors (most terminals support this)
+opt.timeoutlen = 1000                        -- time to wait for a mapped sequence to complete (in milliseconds)
+opt.undofile = true                         -- enable persistent undo
+opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+opt.expandtab = true                        -- convert tabs to spaces
+opt.shiftwidth = 4                          -- the number of spaces inserted for each indentation
+opt.tabstop = 4                             -- insert 2 spaces for a tab
+opt.cursorline = true                       -- highlight the current line
+opt.number = true                           -- set numbered lines
+opt.relativenumber = true                  -- set relative numbered lines
+opt.numberwidth = 4                         -- set number column width to 2 {default 4}
+opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
+opt.wrap = true                             -- display lines as one long line
+opt.linebreak = true                        -- companion to wrap, don't split words
+opt.scrolloff = 8                           -- minimal number of screen lines to keep above and below the cursor
+opt.sidescrolloff = 8                       -- minimal number of screen columns either side of cursor if wrap is `false`
+opt.whichwrap = "bs<>[]hl"                  -- which "horizontal" keys are allowed to travel to prev/next line
+opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
 
-for k, v in pairs(options) do
-  vim.opt[k] = v
-end
-
----  SETTINGS  ---
-vim.opt.spelllang:append "cjk" -- disable spellchecking for asian characters (VIM algorithm does not support it)
-vim.opt.shortmess:append "c" -- don't show redundant messages from ins-completion-menu
-vim.opt.shortmess:append "I" -- don't show the default intro message
-vim.opt.whichwrap:append "<,>,[,],h,l"
-
--- vim.opt.shortmess = "ilmnrx"                        -- flags to shorten vim messages, see :help 'shortmess'
--- vim.opt.iskeyword:append "-"                           -- hyphenated words recognized by searches
--- vim.opt.formatoptions:remove({ "c", "r", "o" })        -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
--- vim.opt.runtimepath:remove("/usr/share/vim/vimfiles")  -- separate vim plugins from neovim in case vim still in use
+opt.spelllang:append "cjk" -- disable spellchecking for asian characters (VIM algorithm does not support it)
+opt.shortmess:append "c" -- don't show redundant messages from ins-completion-menu
+opt.shortmess:append "I" -- don't show the default intro message
+opt.whichwrap:append "<,>,[,],h,l"
 
